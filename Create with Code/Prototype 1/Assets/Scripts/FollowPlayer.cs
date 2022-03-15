443ae3bd33d47ad388374bb602b3a050ee7e70e6
Gary Mayfield <gary.mayfield@hotmail.co.uk>
2022-03-15 22:07:40 +0000

    Finished 1.4

--- Create with Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Create with Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset;
+    private Vector3 offset = new Vector3(0, 6, -11);
 
     // Start is called before the first frame update
     void Start()
