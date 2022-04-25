511623d7e85d6eb709d216c675c5127a0c2c47f2
Gary Mayfield <gary.mayfield@hotmail.co.uk>
2022-04-25 21:10:29 +0100

    Abstract mover class created to reuse logic created for Player

--- Luma/Prototype/Assets/Scripts/Player.cs
+++ Luma/Prototype/Assets/Scripts/Player.cs
@@ -1,55 +1,13 @@
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class Player : Fighter
+public class Player : Mover
 {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
-
-    private void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
-
     private void FixedUpdate()
     {
-
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        // Reset moveDelta
-        moveDelta = new Vector3(x, y, 0);
-
-        // Swap sprite direction, whether you're going right or left
-        if(moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if(moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        // Make sure we can move in y direction, by casting a box there first
-        // If the box returns null, we're free to move
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, 
-            new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), 
-            LayerMask.GetMask("Actor", "Blocking"));
-
-        if(hit.collider == null)
-        {
-            // Move the player
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-        }
-
-        // Make sure we can move in x direction, by casting a box there first
-        // If the box returns null, we're free to move
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, 
-            new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), 
-            LayerMask.GetMask("Actor", "Blocking"));
-
-        if(hit.collider == null)
-        {
-            // Move the player
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
-        }
-
+        UpdateMotor(new Vector3(x, y, 0));
     }
 }
