ff7045658071182cae13a472e0776b5a41b3848e
Gary Mayfield <garymayfield@tfl.gov.uk>
2022-09-14 08:44:01 +0100

    Challenge completed.

---
+++ Create with Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool dogOnCooldown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (!dogOnCooldown && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("SpawnDog");
+        }
+    }
+
+    IEnumerator SpawnDog()
+    {
+        dogOnCooldown = true;
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        yield return new WaitForSeconds(1);
+        dogOnCooldown = false;
+    }
+}
